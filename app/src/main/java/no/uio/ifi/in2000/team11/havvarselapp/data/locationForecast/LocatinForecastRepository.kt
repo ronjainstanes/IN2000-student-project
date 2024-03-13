b05ac5bd60d55191bad48140c7b9b3eda46a0339
Sophie <sophirol@ulrik.uio.no>
2024-03-13 15:35:54 +0100

    Fjernet testmetoder fra main og lagde en egen mappe for det kalt "test"

---
+++ app/src/main/java/no/uio/ifi/in2000/team11/havvarselapp/data/locationForecast/LocatinForecastRepository.kt
@@ -0,0 +1,22 @@
+package no.uio.ifi.in2000.team11.havvarselapp.data.locationForecast
+
+import android.util.Log
+import no.uio.ifi.in2000.team11.havvarselapp.model.locationForecast.LocationForecast
+
+/**
+ * mellomman mellom datasource og Viewmodel for Location Forecast-API'et
+ * må sende inn latitude og longtitude for oprådet data skal hentes fra
+ */
+interface LocationForecastRepository{
+    suspend fun getLocationForecast(lat: String, lon: String): LocationForecast?
+
+}
+class LocatinForecastRepositoryImpl(
+    private val dataKilde: LocationForecastDataSource = LocationForecastDataSource()
+): LocationForecastRepository {
+
+    override suspend fun getLocationForecast(lat: String, lon: String): LocationForecast? {
+        return dataKilde.fetchLocationForecast_LatAndLon(lat, lon)
+    }
+
+}
---
+++ app/src/main/java/no/uio/ifi/in2000/team11/havvarselapp/data/locationForecast/LocationForecastDataSource.kt
@@ -0,0 +1,40 @@
+package no.uio.ifi.in2000.team11.havvarselapp.data.locationForecast
+
+import io.ktor.client.HttpClient
+import io.ktor.client.call.body
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.client.plugins.defaultRequest
+import io.ktor.client.request.get
+import io.ktor.serialization.gson.gson
+import io.ktor.util.appendIfNameAbsent
+import no.uio.ifi.in2000.team11.havvarselapp.model.locationForecast.LocationForecast
+
+/**
+ * Henter LocationForecast-API'et, oppretter et objekt av data klassen LocationForecast, må sende inn Latitude og Longtitude på området data skal hentes fra
+ */
+class LocationForecastDataSource {
+
+    // HTTP client
+    private val client = HttpClient {
+        defaultRequest {
+            url("https://gw-uio.intark.uh-it.no/in2000/")
+            headers.appendIfNameAbsent("X-Gravitee-API-Key", "7c5b6de3-2539-4c5e-bfb3-ec6377399ece")
+        }
+
+        install(ContentNegotiation) {
+            gson()
+        }
+    }
+    suspend fun fetchLocationForecast_LatAndLon(lat: String, lon: String): LocationForecast {
+        val locationforecast: LocationForecast = client.get("https://gw-uio.intark.uh-it.no/in2000/weatherapi/locationforecast/2.0//compact?lat=${lat}&lon=${lon}").body()
+        return locationforecast
+    }
+
+
+}
+
+
+
+
+
+//
