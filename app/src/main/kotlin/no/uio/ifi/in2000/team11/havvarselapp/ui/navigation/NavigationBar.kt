f303cabcd8ae89ea467c866be3864833984ba7c1
juliepe <iuliiape@ulrik.uio.no>
2024-04-23 15:45:30 +0200

    Midlertidig løsning for knapper 2.0

--- app/src/main/kotlin/no/uio/ifi/in2000/team11/havvarselapp/ui/navigation/NavigationBar.kt
+++ app/src/main/kotlin/no/uio/ifi/in2000/team11/havvarselapp/ui/navigation/NavigationBar.kt
@@ -3,10 +3,10 @@ package no.uio.ifi.in2000.team11.havvarselapp.ui.navigation
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.CircleShape
@@ -20,6 +20,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
@@ -34,7 +35,7 @@ var selectedButtonWeather = false
  */
 @Composable
 fun NavigationBarWithButtons(navController: NavController) {
-    val white = Color(69, 79, 92, 255) // button is active
+    val white = Color(69, 79, 92, 167) // button is active
     val gray = Color(19, 35, 44, 255)  // button is inactive
     val boardersWidth = 2.dp
 
@@ -45,84 +46,95 @@ fun NavigationBarWithButtons(navController: NavController) {
         verticalArrangement = Arrangement.Bottom,
     ) {
         Row(
-            horizontalArrangement = Arrangement.Center, // Sentrerer knappene i raden
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(1.dp)
-                .background(gray)
+                .fillMaxWidth() // Ensures that the Row fills up the entire screen width
+                .wrapContentHeight(align = Alignment.Bottom)
         ) {
 
             // button for seamap-screen
             Button(
                 modifier = Modifier
-                    .size(60.dp) // Definerer en eksakt størrelse for knappen
-
                     .weight(1f)
-                    /*.border(
+                    .border(
                         width = boardersWidth,
+                        // changes color if button is active/inactive
                         color = white,
                         shape = RectangleShape
-                    )*/,
+                    ),
                 // when clicked: navigate, change color
                 onClick = {
                     selectedButtonMap = true
                     navController.navigate("seamap_screen")
                     selectedButtonWeather = false
                 },
-                colors = ButtonDefaults.buttonColors(
-                    if (selectedButtonMap) white else gray),
-                shape = CircleShape,
+                colors = ButtonDefaults.buttonColors(gray),
+                shape = RectangleShape,
 
                 ) {
-                ButtonMapContext()
+                ButtonMapContext(selectedButtonMap)
             }
 
             // button for weather-screen
             Button(
                 modifier = Modifier
-                    .size(60.dp) // Definerer en eksakt størrelse for knappen
-
                     .weight(1f)
-                    /*.border(
+                    .border(
                         width = boardersWidth,
                         // changes color if button is active/inactive
                         color = white,
                         shape = RectangleShape
-                    // when clicked: navigate, change color
-                    )*/, onClick = {
+                        // when clicked: navigate, change color
+                    ), onClick = {
                     selectedButtonWeather = true
                     navController.navigate("weather_screen")
                     selectedButtonMap = false
-                }, colors = ButtonDefaults.buttonColors(
-                    if (selectedButtonWeather) white else gray
-                ),
-                shape = CircleShape
+                }, colors = ButtonDefaults.buttonColors(gray), shape = RectangleShape
 
             ) {
-                ButtonWeatherContext()
+                ButtonWeatherContext(selectedButtonWeather)
             }
         }
     }
 }
 
 @Composable
-fun ButtonMapContext() {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Icon(
-            imageVector = Icons.Default.Place,
-            contentDescription = "map"
-        )
-        Text("Kart")
+fun ButtonMapContext(isSelected: Boolean) {
+    val white = Color(100, 110, 125, 200)
+    val gray = Color(19, 35, 44, 255)
+    Box(
+        contentAlignment = Alignment.Center, // Senterer innholdet i Box
+        modifier = Modifier
+            .size(52.dp) // Setter størrelsen på Box, denne må matche størrelsen du setter for Button
+            .clip(CircleShape) // Gjør at Box får en rund form
+            .background(if (isSelected) white else gray) // Endrer bakgrunnsfarge basert på om knappen er valgt
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Icon(
+                imageVector = Icons.Default.Place,
+                contentDescription = "map"
+            )
+            Text("Kart")
+        }
     }
 }
 
 @Composable
-fun ButtonWeatherContext() {
-    Column(horizontalAlignment = Alignment.CenterHorizontally) {
-        Icon(
-            imageVector = Icons.Outlined.WbCloudy,
-            contentDescription = "weather"
-        )
-        Text("Vær")
+fun ButtonWeatherContext(isSelected: Boolean) {
+    val white = Color(100, 110, 125, 200)
+    val gray = Color(19, 35, 44, 255)
+    Box(
+        contentAlignment = Alignment.Center, // Senterer innholdet i Box
+        modifier = Modifier
+            .size(52.dp) // Setter størrelsen på Box, denne må matche størrelsen du setter for Button
+            .clip(CircleShape) // Gjør at Box får en rund form
+            .background(if (isSelected) white else gray) // Endrer bakgrunnsfarge basert på om knappen er valgt
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally) {
+            Icon(
+                imageVector = Icons.Outlined.WbCloudy,
+                contentDescription = "weather"
+            )
+            Text("Vær")
+        }
     }
 }
